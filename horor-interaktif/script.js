aefbcd91c43c7f29f431b14a6c6412a3217f2ef6
peter079707 <silvester.julianusdw@gmail.com>
2024-12-22 01:37:33 +0800

    Delete horor-interaktif directory

--- horor-interaktif/script.js
+++
@@ -1,9 +0,0 @@
-// Tambahkan event listener untuk tombol interaktif
-document.getElementById('startBtn').addEventListener('click', () => {
-    alert('Selamat datang di Pesan dari Alam Gaib! Bersiaplah...');
-  });
-  
-  document.getElementById('learnMoreBtn').addEventListener('click', () => {
-    window.location.href = "#tentang"; // Scroll ke bagian 'Tentang'
-  });
-  
